1b50faa150bc358301a8699b1b07dc670fe6db37
renish-1111 <ponkiyarenish@gmail.com>
2023-11-19 10:26:55 +0530

    v54

---
+++ NEW/video 54/new.js
@@ -0,0 +1,4 @@
+console.log("Hello World")
+
+console.log("Code is Running...")
+console.log("Code is also Running...")
---
+++ NEW/video 54/script.js
@@ -0,0 +1,20 @@
+alert("Hello World");
+
+console.log("Code is Running...")
+console.log("Code is also Running...")
+
+var a = prompt("Enter Your Number")
+
+var isTrue = confirm("Are you sure you want to leave?")
+
+if (isTrue) {
+    console.log("YOU ARE LEAVE IT")
+}
+else {
+
+    console.log("YOU ARE NOT LEAVE IT")
+}
+
+console.log("Your number is " + a)
+
+document.title="Hey"
