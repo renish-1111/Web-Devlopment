bf1358aef10280565e0f2f935ff806fdcb1c203f
renish-1111 <ponkiyarenish@gmail.com>
2023-11-21 15:26:54 +0530

    v55

---
+++ NEW/video 55/script.js
@@ -0,0 +1,39 @@
+console.log("Hey!")
+
+let a = 5;
+a = a + 5;
+let b = 6;
+let c = "Renish";
+
+console.log(a + b + 8)
+
+console.log(typeof a, typeof b, typeof c)
+{
+    let a = 77;
+    console.log(a)
+}
+console.log(a)
+
+let p = 22;
+let q = 22.22;
+let r = "Renish";
+const s = true;
+let t = undefined;
+let u = null;
+
+console.log(p, q, r, s, t, u);
+console.log(typeof p, typeof q, typeof r, typeof s, typeof t, typeof u);
+
+let o ={
+    name:"Renish","job code":1111
+}
+
+console.log(o)
+
+o.salary="100crors";
+
+console.log(o)
+
+o.salary="200crors";
+
+console.log(o)
