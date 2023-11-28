600dd5121fbb72ee1793bc2949bab13d481de0df
renish-1111 <ponkiyarenish@gmail.com>
2023-11-28 19:14:54 +0530

    Create script.js

---
+++ NEW/video 62/script.js
@@ -0,0 +1,21 @@
+let adj = {
+    0:"Fire",
+    1:"Crazy",
+    2:"Amazing",
+};
+
+let mname ={
+    0:"Bad",
+    1:"Food",
+    2:"Motor",
+};
+
+let word ={
+    0:"Bros",
+    1:"Hub",
+    2:"Limited",
+};
+let a=Math.round((Math.random()*2))
+let b=Math.round((Math.random()*2))
+let c=Math.round((Math.random()*2))
+console.log(`${adj[a]} ${mname[b]} ${word[c]}`)
