019d625197037008b49a8328e53f4beb2052dc34
renish-1111 <ponkiyarenish@gmail.com>
2023-11-29 22:17:26 +0530

    add

---
+++ NEW/video 63/script.js
@@ -0,0 +1,32 @@
+let arr = [1,2,3,5,7]
+
+console.log(arr);
+console.log(arr.length);
+console.log(arr[0]);
+console.log(arr[4]);
+arr[0]=8
+console.log(arr);
+
+console.log(arr.toString());
+
+console.log(arr.join(" and "))
+
+console.log(arr.pop)
+
+console.log(arr);
+
+console.log(arr.push("Renish"))
+
+console.log(arr);
+
+console.log(arr.shift)
+
+console.log(arr.unshift("Renish"))
+
+let a1=[1,2,3]
+let a2=[4,5,6]
+let a3=[7,8,9]
+
+console.log(a1.concat(a2,a3))
+
+
