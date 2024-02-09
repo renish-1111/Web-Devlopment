5d488cc4d36b73dbe162afb6ec22f75b644a632e
Renish Ponkiya <ponkiyarenish@gmail.com>
2024-02-09 17:58:31 +0530

    v107

---
+++ NEW/video 107/state-intro/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <div>This is count : {count}</div>
+        <button onClick={() => { setCount(count + 1) }}>Click count +</button>
+      </div>
+    </>
+  )
+}
+
+export default App
