870c24003fb2e58f8355c7f117892f8a9a9f15fd
Renish Ponkiya <ponkiyarenish@gmail.com>
2024-02-11 23:01:47 +0530

    v109

---
+++ NEW/video 109/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import App_1 from './App_1.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <App_1 />
+  // </React.StrictMode>
+)
